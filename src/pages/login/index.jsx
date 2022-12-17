c8aa39a6e596ff05a3adddf96c94d7e913ae7a15
Cingetorix <godoy.dalla.gassa@gmail.com>
2022-12-17 01:30:29 -0300

    Criado tela de cadastro

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,5 @@
 import { useNavigate  } from "react-router-dom";
-import { MdEmail, MdLock } from 'react-icons/md'
+import { MdPerson, MdEmail, MdLock } from 'react-icons/md'
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
@@ -8,7 +8,7 @@ import { api } from '../../services/api';
 import { useForm } from "react-hook-form";
 
 
-import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
+import { Container, Title, Column, TitleLogin, SubtitleLogin, Wrapper, Advice, HasAccount } from './styles';
 
 const Login = () => {
 
@@ -45,19 +45,21 @@ const Login = () => {
             </Column>
             <Column>
                 <Wrapper>
-                <TitleLogin>Faça seu cadastro</TitleLogin>
-                <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                <TitleLogin>Comece agora grátis</TitleLogin>
+                <SubtitleLogin>Crise sua conta e make the change._</SubtitleLogin>
                 <form onSubmit={handleSubmit(onSubmit)}>
+                <Input placeholder="Nome completo" leftIcon={<MdPerson />} name="text"  control={control} />
                     <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
                     {errors.email && <span>E-mail é obrigatório</span>}
                     <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
                     {errors.senha && <span>Senha é obrigatório</span>}
                     <Button title="Entrar" variant="secondary" type="submit"/>
                 </form>
-                <Row>
-                    <EsqueciText>Esqueci minha senha</EsqueciText>
-                    <CriarText>Criar Conta</CriarText>
-                </Row>
+                <Advice>Ao clicar em "criar minha conta grátis", declaro que 
+aceito as Políticas de Privacidade e os Termos de Uso da DIO.
+
+</Advice>
+<HasAccount> Já tenho conta. <span style={{color: '#23cf69',fontWeight: 'bold'}}>Fazer login.</span> </HasAccount>
                 </Wrapper>
             </Column>
         </Container>
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
 
+
+
+export const Advice = styled.p`
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 25px;
+    margin-top: 35px;
+    font-family: 'Open Sans';
+
+`
+export const HasAccount = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 13.1px;
+    line-height: 25px;
+    margin-top: 5px;
+    margin-bottom: 35px;
+
+` 
+
 export const Container = styled.main`
     width: 100%;
     max-width: 80%;
@@ -13,7 +35,7 @@ export const Container = styled.main`
 `
 
 export const Wrapper = styled.div`
-    max-width: 300px;
+max-width: 330px;
 `
 
 export const Column = styled.div`
@@ -28,55 +50,32 @@ export const Row = styled.div`
     margin-top: 20px;
 `
 
-
-
 export const Title = styled.h2`
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 32px;
-    max-width: 90%;
-    margin-bottom: 20px;
-    line-height: 44px;
-
-    color: #FFFFFF;
+font-family: 'Open Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 32px;
+max-width: 80%;
+margin-bottom: 20px;
+line-height: 44px;
+color: #FFFFFF;
+margin-top: 10px;
 `
 
 export const TitleLogin = styled.p`
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 32px;
-    line-height: 44px;
-    margin-bottom: 8px;
+font-family: 'Open Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 32px;
+line-height: 44px;
+margin-bottom: 8px;
 `
 
 export const SubtitleLogin = styled.p`
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 18px;
-    line-height: 25px;
-    margin-bottom: 35px;
-`
-
-
-export const EsqueciText  = styled.p`
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 14px;
-    line-height: 19px;
-
-    color: #E5E044;
-` 
-
-export const CriarText =  styled.p`
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 14px;
-    line-height: 19px;
-
-    color: #23DD7A;
+font-family: 'Open Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 25px;
+margin-bottom: 35px;
 `
